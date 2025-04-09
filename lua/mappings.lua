0356459975af028a16a4d2b77df3667b3bc92739
Dimitar Stoev <dimitarg.stoev@gmail.com>
2025-04-09 16:24:04 +0300

    Removed copilot.vim from tracking and updated .gitignore

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,11 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true})
+
+vim.g.copilot_filetypes = {
+  ["*"] = true,
+  ["gitcommit"] = false,
+}
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,7 +12,19 @@ return {
       require "configs.lspconfig"
     end,
   },
-
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()
+      -- Mapping tab is already used by NvChad
+      vim.g.copilot_no_tab_map = true;
+      vim.g.copilot_assume_mapped = true;
+      vim.g.copilot_tab_fallback = "";
+      -- The mapping is set to other key, see custom/lua/mappings
+      -- or run <leader>ch to see copilot mapping section
+      -- it is mapped to <C-J> in lua/mappings.lua
+    end
+  },
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
